785b85c7c556760fcab3a34de51eed09ecb15ad5
lashasuxa <lashasukhashvili1995@gmail.com>
2023-06-21 23:14:17 -0400

    feat:get and post request

--- src/controllers/project-controller.js
+++ src/controllers/project-controller.js
@@ -1,20 +1,30 @@
 import Todo from "../models/Todo.js";
 
-export const getAllTodo = async (req, res) => {
+export const getAllTodos = async (req, res) => {
   const data = await Todo.find();
-
-  return res.status(200).json(data);
-};
-
-export const getWebTodos = async (req, res) => {
-  const data = await Todo.find({ category: "web" });
-
   return res.status(200).json(data);
 };
 
 export const addTodo = async (req, res) => {
-  const { todo, status } = req.body;
-  let id = "3";
-  await Todo.create({ todo, id, status });
-  return res.status(200).json({ todo, id, status });
+  const { todo, status, category } = req.body;
+  const newTodo = new Todo({ todo, status, category });
+  await newTodo.save();
+  return res.status(200).json(newTodo);
+};
+
+export const deleteTodo = async (req, res) => {
+  const { id } = req.params;
+  await Todo.findByIdAndRemove(id);
+  return res.status(200).json({ message: "Todo deleted successfully." });
+};
+
+export const updateTodo = async (req, res) => {
+  const { id } = req.params;
+  const { todo, status, category } = req.body;
+  const updatedTodo = await Todo.findByIdAndUpdate(
+    id,
+    { todo, status, category },
+    { new: true }
+  );
+  return res.status(200).json(updatedTodo);
 };
